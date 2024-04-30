19a53b74352315f3b2a326b2474019af72801be2
ALI ALI <alirafikali22@gmail.com>
2024-04-30 09:49:21 +0300

    fix bugs

--- app/Policies/CustomerPolicy.php
+++ app/Policies/CustomerPolicy.php
@@ -13,7 +13,7 @@ class CutomerPolicy
      */
     public function viewAny(User $user): bool
     {
-        //
+       return false;
     }
 
     /**
@@ -21,7 +21,7 @@ class CutomerPolicy
      */
     public function view(User $user, Cutomer $cutomer): bool
     {
-        //
+        return false;
     }
 
     /**
@@ -29,7 +29,7 @@ class CutomerPolicy
      */
     public function create(User $user): bool
     {
-        //
+        return false;
     }
 
     /**
@@ -37,7 +37,7 @@ class CutomerPolicy
      */
     public function update(User $user, Cutomer $cutomer): bool
     {
-        //
+        return false;
     }
 
     /**
@@ -45,7 +45,7 @@ class CutomerPolicy
      */
     public function delete(User $user, Cutomer $cutomer): bool
     {
-        //
+        return false;
     }
 
     /**
@@ -53,7 +53,7 @@ class CutomerPolicy
      */
     public function restore(User $user, Cutomer $cutomer): bool
     {
-        //
+        return false;
     }
 
     /**
@@ -61,6 +61,6 @@ class CutomerPolicy
      */
     public function forceDelete(User $user, Cutomer $cutomer): bool
     {
-        //
+        return false;
     }
 }
--- database/migrations/2024_04_28_073107_create_invoices_table.php
+++ database/migrations/2024_04_28_073107_create_invoices_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
+            $table->integer('customer_id');
+            $table->integer('amount');
+            $table->string('status');
+            $table->string('status');
+            $table->string('status');
+
             $table->timestamps();
         });
     }
