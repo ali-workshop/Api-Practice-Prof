7d1a7a10cca1e01874f61f3c09084ae28cf623fc
ALI ALI <alirafikali22@gmail.com>
2024-04-30 09:59:18 +0300

    build the factory customer

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Cutomer>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
  */
-class CutomerFactory extends Factory
+class CustomerFactory extends Factory
 {
     /**
      * Define the model's default state.
