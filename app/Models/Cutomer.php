26bea23391187ed8ba8a67aeac68182ccec9c8d8
ALI ALI <alirafikali22@gmail.com>
2024-04-30 09:41:05 +0300

    build the realtions

--- app/Models/Cutomer.php
+++ app/Models/Cutomer.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Cutomer extends Model
 {
     use HasFactory;
+
+
+public function invoices():HasMany{
+
+
+    return $this->hasMany(Invoices::class);
+
+
+
+}
+
+
+
 }
